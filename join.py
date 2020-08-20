afc530954ec310a4b841bc07f3d8b4a300d03980
ImpulseAI <68378591+ImpulseAI@users.noreply.github.com>
2020-08-20 20:06:46 +0530

    join.py

---
+++ join.py
@@ -0,0 +1,20 @@
+
+import os
+def join(files, dest_file):
+    output_file = open(dest_file, 'wb')
+    for file in files:
+        print('Joining',file,'...')
+        input_file = open(file, 'rb')
+        while True:
+            byte = input_file.read(1000000)
+            if not byte:
+                break
+            output_file.write(byte)
+        input_file.close()
+    output_file.close()
+    for file in files:
+        print('Removing',file,'...')
+        os.remove(file)
+join(['ntscu.zip.000.COOL'],'ntscu.zip')
+with zipfile.ZipFile('ntscu.zip', 'r') as zip_ref:
+    zip_ref.extractall(ntscu)
